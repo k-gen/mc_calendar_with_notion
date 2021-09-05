1ffaca323c9f6a9eb144c8be95e72d1093ac09c6
Yuki Kagen <kagen.yuki@gmail.com>
2021-09-05 16:47:17 +0900

    当番日付をレコードに追加する際に休日を除外する処理を追加&コード微調整

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,23 @@
 import { Client, UnknownHTTPResponseError } from "@notionhq/client"
+import { DatePropertyValue } from '@notionhq/client/build/src/api-types'
 import dotenv from "dotenv"
 dotenv.config()
-import dayjs from 'dayjs';
-import weekday from 'dayjs/plugin/weekday.js'
+import dayjs, { Dayjs } from 'dayjs';
 import 'dayjs/locale/ja.js'
-dayjs.extend(weekday)
+import holiday_jp from '@holiday-jp/holiday_jp';
 
 const notion = new Client({ auth: process.env.NOTION_KEY })
 
 const databaseId = process.env.NOTION_DATABASE_ID
 
 /**
- * ページIDのリストを取得
+ * 各レコード行のID(ページID)のリストを取得
  * @returns {Array} - pageIds
  */
 const getPageIds = async () => {
     try {
         const response = await notion.databases.query({
             database_id: databaseId != null ? databaseId : "",
-            sorts: [
-              {
-                property: 'Name',
-                direction: 'ascending',
-              },
-            ],
         });
         const pageIds = response.results.map(result => {
             return result.id
@@ -37,17 +31,20 @@ const getPageIds = async () => {
 }
 
 /**
- * Dateカラムのレコードが今日の日付と一致しているかの判定
+ * 対象レコードのDateカラムの値が今日の日付と一致しているか判定
  * @param {string} pageId - page_id
  * @param {string} today - YYYY-MM-DD
  * @returns {boolean} レコードとtodayが一致しているか
  */
-const isMatchOfToday = async (pageId: string, today: string) => {
+const isToday = async (pageId: string, today: string) => {
     try {
         const response = await notion.pages.retrieve({
             page_id: pageId,
         });
-        return response.properties.Date.date.start === today
+        const dateProperty = response.properties.Date as DatePropertyValue
+        if (dateProperty.date != null) {
+            return dateProperty.date.start === today
+        }
     } catch (error) {
         if (error instanceof UnknownHTTPResponseError) {
             console.log(error.body)
@@ -56,14 +53,51 @@ const isMatchOfToday = async (pageId: string, today: string) => {
 }
 
 /**
+ * 引数の日付が休日であるか判定
+ * @param {string} date - date
+ * @returns {boolean} 休日ならtrue
+ */
+const isHoliday = (date) => {
+    const isSaturday = dayjs(date).day() === 6
+    const isSunday = dayjs(date).day() === 0
+    return holiday_jp.isHoliday(new Date(date)) || isSaturday || isSunday
+}
+
+/**
+ * 平日のリストを取得
+ * @param dayjs
+ * @returns {Array} weekdays
+ */
+const getWeekdays = (dayjs: Dayjs) => {
+    let weekdays:string[] = []
+    for (let i = 0; i < dayjs.daysInMonth(); i++) {
+        let date = dayjs.add(i, 'day')
+        let formattedDate = date.locale('ja').format('YYYY-MM-DD')
+        if (!isHoliday(formattedDate) && dayjs.month() === date.month()) {
+            weekdays.push(formattedDate)
+        }
+    }
+    return weekdays
+}
+
+/**
  * 翌週の日直当番の日付をレコードに追加
  * @returns {void}
  */
 const updateContentForDate = async () => {
     try {
+        // 各レコード行のidを取得
         const pageIds = await getPageIds()
         if (pageIds == null || pageIds.length === 0) return
-        pageIds.forEach( async (pageId, index) => {            
+        // 当月の平日のみのリストを取得
+        let weekdays = getWeekdays(dayjs())
+        // 当月の平日の数がレコード行数に満たない場合は来月分も取得
+        if (pageIds.length > weekdays.length) {
+            let weekdaysInNextMonth = getWeekdays(dayjs().date(1).add(1, 'month'))
+            weekdays = [...weekdays,...weekdaysInNextMonth]
+        }
+        // 各レコード行に日付を追加
+        pageIds.forEach( async (pageId, index) => {
             const response = await notion.pages.update({
                 page_id: pageId,
                 archived: false,
@@ -71,7 +105,7 @@ const updateContentForDate = async () => {
                     "Date": {
                         type: 'date',
                         date: {
-                            start: dayjs().weekday(index + 8).locale('ja').format('YYYY-MM-DD'),
+                            start: weekdays[index],
                             end: undefined
                         }
                     }
@@ -87,12 +121,17 @@ const updateContentForDate = async () => {
     }
 }
 
+/**
+ * 当日が日直のメンバーにタグを追加する
+ * @param today 
+ * @returns {void}
+ */
 const updateContentForTags = async (today: string) => {
     try {
         const pageIds = await getPageIds()
         if (pageIds == null || pageIds.length === 0) return
         pageIds.forEach( async (pageId) => {               
-            if (await isMatchOfToday(pageId, today)) {
+            if (await isToday(pageId, today)) {
                 const response = await notion.pages.update({
                     page_id: pageId,
                     archived: false,
