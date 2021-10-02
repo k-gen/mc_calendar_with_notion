16d37f998134f12e74623fe4dc073cd4e1a55169
Yuki Kagen <kagen.yuki@gmail.com>
2021-10-02 15:47:23 +0900

    test: ✅ passed tests and add mock data

---
+++ src/__tests__/query_pages.test.ts
@@ -0,0 +1,39 @@
+import { notion } from '../config'
+import * as query_pages from "../modules/query_pages"
+import * as mock from "./mock"
+
+describe("query_pages.queryDiffContents", () => {
+    let httpRequestGetMock: jest.SpyInstance;
+    beforeEach(() => {
+        httpRequestGetMock = jest.spyOn(notion.databases, "query")
+        httpRequestGetMock
+            .mockResolvedValue(mock.pages)
+            .mockResolvedValueOnce(mock.pages)
+            .mockResolvedValueOnce({results: []});
+    })
+    // test("0", async () => {
+    //     await query_pages.queryDiffContents([], Infinity)
+    // })
+    test("1", async () => {
+        let pages = await query_pages.queryPages()
+        const result = await query_pages.queryDiffContents(pages, 3)
+        expect(pages).toEqual(mock.pages.results)
+        expect(result.length).toBe(3)
+        expect(result).toStrictEqual(["ももこ","ののか","ようこ"])
+    })
+})
+
+// describe("query_pages.queryDeletePages", () => {
+//     let httpRequestGetMock: jest.SpyInstance;
+//     beforeEach(() => {
+//         httpRequestGetMock = jest.spyOn(notion.databases, "query")
+//         httpRequestGetMock.mockResolvedValue(mock.pages)
+//     })        
+//     test("0", async () => {
+//         await query_pages.queryDeletePages([])
+//     })
+//     test("1", async () => {
+//         let pages = await query_pages.queryPages()
+//         await query_pages.queryDeletePages(pages)
+//     })
+// })
--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -1,25 +1,15 @@
 import { notion } from '../config'
 import * as utils from "../utils"
-
-const mockData = {
-    page_id: '123456789',
-    properties: {
-        Date: {
-            date: {
-                start: "2021-09-30"
-            }
-        }
-    }
-}
+import * as mock from "./mock"
 
 describe("utils.isToday", () => {
     let httpRequestGetMock: jest.SpyInstance;
     beforeEach(() => {
         httpRequestGetMock = jest.spyOn(notion.pages, "retrieve")
-        httpRequestGetMock.mockResolvedValue(mockData);
+        httpRequestGetMock.mockResolvedValue(mock.today)
     })
     test("0", async () => {
-        const result = await utils.isToday("123456789", "2021-09-30")
+        const result = await utils.isToday("b0b1467f-b5c1-40f2-8d9c-3576e8c28be5", "2021-09-30")
         expect(result).toBe(true)
     })
 })
