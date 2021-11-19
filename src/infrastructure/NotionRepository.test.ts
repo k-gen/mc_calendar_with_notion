c295b55b5d76f82aec064163fc4d4dde71c474a2
shzawa <ozawa.shotaro0114@gmail.com>
2021-11-20 01:04:08 +0900

    update: beforeEachでNotionRepositoryを発行するようにした

--- src/infrastructure/NotionRepository.test.ts
+++ src/infrastructure/NotionRepository.test.ts
@@ -1,12 +1,15 @@
 import { DatabasesQueryResponse, PagesRetrieveResponse } from "@notionhq/client/build/src/api-endpoints";
-import Client from "@notionhq/client/build/src/Client";
 import { dayjsJa } from "../utils";
 import { page, pages } from "../__tests__/mock";
 import { NotionRepository } from "./NotionRepository";
 
 describe('NotionRepository', () => {
+  let notionRepository: NotionRepository
+
   beforeEach(() => {
     jest.resetModules() // Most important - it clears the cache
+    jest.spyOn(global.console, 'log').mockImplementation();
+    notionRepository = new NotionRepository()
   });
   afterAll(() => {
     jest.restoreAllMocks();
@@ -18,9 +21,7 @@ describe('NotionRepository', () => {
 
   describe('getPages()', () => {
     test('getPages(): success', async () => {
-      const notionRepository = new NotionRepository()
       const querySpy = jest.spyOn(notionRepository, 'query').mockImplementation((): Promise<DatabasesQueryResponse> => Promise.resolve(pages as DatabasesQueryResponse))
-
       const result = await notionRepository.getPageIds()
       if (!result) {
         throw new Error('The results property of the page mock is empty');
@@ -32,8 +33,7 @@ describe('NotionRepository', () => {
   })
 
   describe('isToday()', () => {
-    test('isToday(): failed: DateProperty is undefined', async () => {
-      const notionRepository = new NotionRepository()
+    test('isToday(): failed: Date Prop Name is not found.', async () => {
       const retrieveSpy = jest.spyOn(notionRepository, 'retrieve').mockImplementation((_: { pageId: string }): Promise<PagesRetrieveResponse> => {
         delete page.properties.Date
         return Promise.resolve({
@@ -44,10 +44,12 @@ describe('NotionRepository', () => {
         } as unknown as PagesRetrieveResponse)
       })
       const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation(() => {})
-
       await notionRepository.isToday('', dayjsJa())
       expect(retrieveSpy).toHaveBeenCalled()
       expect(consoleErrorSpy).toHaveBeenCalled()
     })
+
+    // test('isToday(): failed: Date Prop Name is not a Date.', () => {
+    // })
   })
 })
